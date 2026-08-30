f260de9c7b4dd6e0f54119e2d7b915f362377a85
Joseeptessele <josetesselep@gmail.com>
2026-08-30 15:40:57 -0300

    feat(mux): enhance WebAPI to include logging middleware for request tracking

--- apis/services/sales/mux/mux.go
+++ apis/services/sales/mux/mux.go
@@ -4,12 +4,14 @@ import (
 	"os"
 
 	"github.com/Joseeptessele/ardanlabs-service/apis/services/sales/route/sys/checkapi"
+	"github.com/Joseeptessele/ardanlabs-service/app/api/mid"
+	"github.com/Joseeptessele/ardanlabs-service/foundation/logger"
 	"github.com/Joseeptessele/ardanlabs-service/foundation/web"
 )
 
 // WebAPIAuth constructs a http.Handler with all application routes bound.
-func WebAPI(shutdown chan os.Signal) *web.App {
-	mux := web.NewApp(shutdown)
+func WebAPI(log *logger.Logger, shutdown chan os.Signal) *web.App {
+	mux := web.NewApp(shutdown, mid.Logger(log))
 
 	checkapi.Routes(mux)
 
